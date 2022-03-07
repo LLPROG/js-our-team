cb1bb9cb7bfd2a592da0351b5ec912676a443efd
LLPROG <94935580+LLPROG@users.noreply.github.com>
2022-03-07 16:52:03 +0000

    cards printed without input

--- script.js
+++ script.js
@@ -23,64 +23,72 @@ Buon lavoro!
 Prendendo come riferimento la card di esempio presente nell'html, stampiamo dinamicamente una card per ogni membro del team.
 */
 
-const teamContainer = document.querySelector('.team-container');
-const teamCard = document.querySelector('.team-card');
-const cardImage = document.querySelector('.team-card');
-const cardText = document.querySelector('.card-text');
-
-const nameMember = document.getElementById('title');
-const roleMember = document.getElementById('role');
 
 
-// for (let i = 0; i < arrMemberTeam.length; i++) {
 
-//     for(let object in arrMemberTeam[i]) {
+const arrMemberTeam = [
 
-//     }
-
-// }
-
-let arrMemberTeam = [
     {
-        photo: '<img src="https://picsum.photos/400/420" alt="Wayne Barnett" />',
-        name: 'wayne Bernett',
-        role: 'Founder & CEO'
-    },
-    {
-        photo:`<img src="img/wayne-barnett-founder-ceo.jpg" alt="Wayne Barnett" />`,
+        photo: '<img src="https://picsum.photos/400/420"/>',
         name: 'wayne Bernett',
         role: 'Founder & CEO',
     },
     {
-        photo:`<img src="https://picsum.photos/400/429/" alt="Wayne Barnett" />`,
-        name: 'wayne Bernett',
-        role: 'Founder & CEO',
+        photo: '<img src="https://picsum.photos/400/420"/>',
+        name: 'Angela Carrol',
+        role: 'Chief Editor',
     },
     {
-        photo:`<img src="img/wayne-barnett-founder-ceo.jpg" alt="Wayne Barnett" />`,
-        name: 'wayne Bernett',
-        role: 'Founder & CEO',
+        photo: '<img src="https://picsum.photos/400/420"/>',
+        name: 'walter Gordon',
+        role: 'Office Manager',
     },
     {
-        photo:`<img src="img/wayne-barnett-founder-ceo.jpg" alt="Wayne Barnett" />`,
-        name: 'wayne Bernett',
-        role: 'Founder & CEO',
+        photo: '<img src="https://picsum.photos/400/420"/>',
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
     },
     {
-        photo:`<img src="img/wayne-barnett-founder-ceo.jpg" alt="Wayne Barnett" />`,
-        name: 'wayne Bernett',
-        role: 'Founder & CEO',
+        photo: '<img src="https://picsum.photos/400/420"/>',
+        name: 'Scott Estrada',
+        role: 'Developer',
+    },
+    {
+        photo: '<img src="https://picsum.photos/400/420"/>',
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
     },
 ];
 
+const teamContainer = document.querySelector('.team-container');
 
-cardImage.innerHTML = arrMemberTeam[0]['photo'];
-nameMember.innerHTML = arrMemberTeam[0]['name'];
-roleMember.innerHTML = arrMemberTeam[0]['role'];
 
-// console.log(arrMemberTeam[0].photo);
-// console.log(arrMemberTeam[0].name);
-// console.log(arrMemberTeam[0].role);
+
+for(let i = 0; i < arrMemberTeam.length; i++) {
+
+    let card = document.createElement('div');
+    card.classList.add('team-card');
+    card.innerHTML = `<div class="card-image">
+ 
+    </div>
+    <div class="card-text">
+      <h3 id="title"></h3>
+      <p id="role"></p>
+    </div>`;
+
+    teamContainer.append(card);
+}
+
+const cardImage = document.querySelectorAll('.card-image');
+const nameMember = document.querySelectorAll('#title');
+const roleMember = document.querySelectorAll('#role');
+
+for (let i = 0; i < arrMemberTeam.length; i++) {
+    cardImage[i].innerHTML = arrMemberTeam[i].photo
+    nameMember[i].innerHTML = arrMemberTeam[i].name;
+    roleMember[i].innerHTML = arrMemberTeam[i].role;
+}
+
 
 
 
