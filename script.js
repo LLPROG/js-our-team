74a1a22b1e1317a3bc68e16cd9e1e5f628c46478
LLPROG <94935580+LLPROG@users.noreply.github.com>
2022-03-07 18:35:57 +0000

    add comments

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
-/*
 
-cosegna
-
-Viene fornito un layout di base. Come prima cosa nel file js definitevi un array di oggetti che rappresentano i membro del team (prendete i dati dallo screen fornito). Ogni membro deve avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
-Prendendo come riferimento la card di esempio presente nell'html, stampiamo dinamicamente una card per ogni membro del team.
-BONUS:
-Utilizziamo gli input presenti nella pagina per permettere all'utente di aggiungere nuovi membri del team: cliccando sul pulsante "add" viene creato un nuovo oggetto, il quale viene inserito nell'array iniziale, e viene stampata una nuova card con tutte le informazioni inserite dall'utente.
-Consigli del giorno:
-Ragioniamo come sempre a step.
-Prima la logica in italiano e poi traduciamo in codice.
-console.log() è nostro amico.
-Quando un pezzo di codice funziona, chiediamoci se possiamo scomporlo in funzioni più piccole.
-Buon lavoro!
-
-*/
 
 /// definitevi un array di oggetti che rappresentano i membro del team 
 
-
-
 /*
 Prendendo come riferimento la card di esempio presente nell'html, stampiamo dinamicamente una card per ogni membro del team.
 */
-
-
-
 
 // const arrMemberTeam = [
 
@@ -60,10 +40,10 @@ Prendendo come riferimento la card di esempio presente nell'html, stampiamo dina
 //     },
 // ];
 
+
+/// const out fo functions
 const arrMemberTeam = [];
-
 const teamContainer = document.querySelector('.team-container');
-
 const addMemberbtn = document.querySelector('#addMemberButton');
 
 
@@ -114,7 +94,6 @@ addMemberbtn.addEventListener('click', () => {
     let inputName = document.querySelector('#name');
     let inputRole = document.querySelector('#role');
     let inputImage = document.querySelector('#image');
-
 
     inputName.value = '';
     inputRole.value = '';
