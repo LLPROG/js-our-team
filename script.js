78859b5ed7cba1923dd55798fd98912e828d22dc
LLPROG <94935580+LLPROG@users.noreply.github.com>
2022-03-07 18:21:06 +0000

    print cards from input

--- script.js
+++ script.js
@@ -26,73 +26,99 @@ Prendendo come riferimento la card di esempio presente nell'html, stampiamo dina
 
 
 
-const arrMemberTeam = [
+// const arrMemberTeam = [
 
-    {
-        photo: '<img src="https://picsum.photos/400/420"/>',
-        name: 'wayne Bernett',
-        role: 'Founder & CEO',
-    },
-    {
-        photo: '<img src="https://picsum.photos/400/420"/>',
-        name: 'Angela Carrol',
-        role: 'Chief Editor',
-    },
-    {
-        photo: '<img src="https://picsum.photos/400/420"/>',
-        name: 'walter Gordon',
-        role: 'Office Manager',
-    },
-    {
-        photo: '<img src="https://picsum.photos/400/420"/>',
-        name: 'Angela Lopez',
-        role: 'Social Media Manager',
-    },
-    {
-        photo: '<img src="https://picsum.photos/400/420"/>',
-        name: 'Scott Estrada',
-        role: 'Developer',
-    },
-    {
-        photo: '<img src="https://picsum.photos/400/420"/>',
-        name: 'Barbara Ramos',
-        role: 'Graphic Designer',
-    },
-];
+//     {
+//         photo: '<img src="https://picsum.photos/400/420"/>',
+//         name: 'wayne Bernett',
+//         role: 'Founder & CEO',
+//     },
+//     {
+//         photo: '<img src="https://picsum.photos/400/420"/>',
+//         name: 'Angela Carrol',
+//         role: 'Chief Editor',
+//     },
+//     {
+//         photo: '<img src="https://picsum.photos/400/420"/>',
+//         name: 'walter Gordon',
+//         role: 'Office Manager',
+//     },
+//     {
+//         photo: '<img src="https://picsum.photos/400/420"/>',
+//         name: 'Angela Lopez',
+//         role: 'Social Media Manager',
+//     },
+//     {
+//         photo: '<img src="https://picsum.photos/400/420"/>',
+//         name: 'Scott Estrada',
+//         role: 'Developer',
+//     },
+//     {
+//         photo: '<img src="https://picsum.photos/400/420"/>',
+//         name: 'Barbara Ramos',
+//         role: 'Graphic Designer',
+//     },
+// ];
+
+const arrMemberTeam = [];
 
 const teamContainer = document.querySelector('.team-container');
 
+const addMemberbtn = document.querySelector('#addMemberButton');
 
 
-for(let i = 0; i < arrMemberTeam.length; i++) {
+//// function create cards
+addMemberbtn.addEventListener('click', () => {
 
+    ///// valus input
+    let inputName = document.querySelector('#name').value;
+    let inputRole = document.querySelector('#role').value;
+    let inputImage = document.querySelector('#image').value;
+
+    /// push of valus in arrMemberTeam
+    arrMemberTeam.push({
+        name: inputName,
+        role: inputRole,
+        image: `<img src="${inputImage}" />`,
+    });
+
+    /// create element card
     let card = document.createElement('div');
     card.classList.add('team-card');
     card.innerHTML = `<div class="card-image">
  
     </div>
     <div class="card-text">
-      <h3 id="title"></h3>
-      <p id="role"></p>
+      <h3 class="title"></h3>
+      <p class="role"></p>
     </div>`;
 
     teamContainer.append(card);
-}
 
-const cardImage = document.querySelectorAll('.card-image');
-const nameMember = document.querySelectorAll('#title');
-const roleMember = document.querySelectorAll('#role');
+    /// take proprerty from arrey
+    for (let i = 0; i < arrMemberTeam.length; i++) {
 
-for (let i = 0; i < arrMemberTeam.length; i++) {
-    cardImage[i].innerHTML = arrMemberTeam[i].photo
-    nameMember[i].innerHTML = arrMemberTeam[i].name;
-    roleMember[i].innerHTML = arrMemberTeam[i].role;
-}
+        const cardImage = document.querySelectorAll('.card-image');
+        const nameMember = document.querySelectorAll('.title');
+        const roleMember = document.querySelectorAll('.role');
+    
+        cardImage[i].innerHTML = arrMemberTeam[i].image
+        nameMember[i].innerHTML = arrMemberTeam[i].name;
+        roleMember[i].innerHTML = arrMemberTeam[i].role;
+    }
+
+});
+
+//// function clear form
+addMemberbtn.addEventListener('click', () => {
+    let inputName = document.querySelector('#name');
+    let inputRole = document.querySelector('#role');
+    let inputImage = document.querySelector('#image');
 
 
+    inputName.value = '';
+    inputRole.value = '';
+    inputImage.value = '';
 
-
-
-
-
+})
 
