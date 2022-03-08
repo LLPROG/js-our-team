129f4f98f2feda0b4f0e533612a477f2efdc154b
LLPROG <94935580+LLPROG@users.noreply.github.com>
2022-03-08 09:48:17 +0000

    adding functions

--- script.js
+++ script.js
@@ -6,39 +6,41 @@
 Prendendo come riferimento la card di esempio presente nell'html, stampiamo dinamicamente una card per ogni membro del team.
 */
 
-// const arrMemberTeam = [
+/* arr member team
+const arrMemberTeam = [
 
-//     {
-//         photo: '<img src="https://picsum.photos/400/420"/>',
-//         name: 'wayne Bernett',
-//         role: 'Founder & CEO',
-//     },
-//     {
-//         photo: '<img src="https://picsum.photos/400/420"/>',
-//         name: 'Angela Carrol',
-//         role: 'Chief Editor',
-//     },
-//     {
-//         photo: '<img src="https://picsum.photos/400/420"/>',
-//         name: 'walter Gordon',
-//         role: 'Office Manager',
-//     },
-//     {
-//         photo: '<img src="https://picsum.photos/400/420"/>',
-//         name: 'Angela Lopez',
-//         role: 'Social Media Manager',
-//     },
-//     {
-//         photo: '<img src="https://picsum.photos/400/420"/>',
-//         name: 'Scott Estrada',
-//         role: 'Developer',
-//     },
-//     {
-//         photo: '<img src="https://picsum.photos/400/420"/>',
-//         name: 'Barbara Ramos',
-//         role: 'Graphic Designer',
-//     },
-// ];
+    {
+        photo: '<img src="https://picsum.photos/400/420"/>',
+        name: 'wayne Bernett',
+        role: 'Founder & CEO',
+    },
+    {
+        photo: '<img src="https://picsum.photos/400/420"/>',
+        name: 'Angela Carrol',
+        role: 'Chief Editor',
+    },
+    {
+        photo: '<img src="https://picsum.photos/400/420"/>',
+        name: 'walter Gordon',
+        role: 'Office Manager',
+    },
+    {
+        photo: '<img src="https://picsum.photos/400/420"/>',
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+    },
+    {
+        photo: '<img src="https://picsum.photos/400/420"/>',
+        name: 'Scott Estrada',
+        role: 'Developer',
+    },
+    {
+        photo: '<img src="https://picsum.photos/400/420"/>',
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+    },
+];
+*/
 
 
 /// const out fo functions
@@ -46,58 +48,73 @@ const arrMemberTeam = [];
 const teamContainer = document.querySelector('.team-container');
 const addMemberbtn = document.querySelector('#addMemberButton');
 
+///// valus input
+const inputName = document.querySelector('#name');
+const inputRole = document.querySelector('#role');
+const inputImage = document.querySelector('#image');
 
-//// function create cards
-addMemberbtn.addEventListener('click', () => {
+//// function on click
+addMemberbtn.addEventListener('click', addMember);
 
-    ///// valus input
-    let inputName = document.querySelector('#name').value;
-    let inputRole = document.querySelector('#role').value;
-    let inputImage = document.querySelector('#image').value;
+//// function add member 
+function addMember() {
 
-    /// push of valus in arrMemberTeam
-    arrMemberTeam.push({
-        name: inputName,
-        role: inputRole,
-        image: `<img src="${inputImage}" />`,
-    });
+        /// push of valus in arrMemberTeam
+        arrMemberTeam.push({
+            name: inputName.value,
+            role: inputRole.value,
+            image: `<img src="${inputImage.value}"/>`,
+        });
+    
+        console.log(arrMemberTeam)
+        createMemberCard();
+        clearForm();
+        takepropertyArrey();
+}
 
-    /// create element card
+/// function create member card
+function createMemberCard() {
+
     let card = document.createElement('div');
     card.classList.add('team-card');
-    card.innerHTML = `<div class="card-image">
- 
-    </div>
-    <div class="card-text">
-      <h3 class="title"></h3>
-      <p class="role"></p>
-    </div>`;
+    card.innerHTML =`<div class="card-image">
 
-    teamContainer.append(card);
+                    </div>
+                    <div class="card-text">
+                        <h3 class="title"></h3>
+                        <p class="role"></p>
+                    </div>`;
 
-    /// take proprerty from arrey
-    for (let i = 0; i < arrMemberTeam.length; i++) {
+    card.addEventListener('click', deleteCard);
+    return teamContainer.append(card);
 
-        const cardImage = document.querySelectorAll('.card-image');
-        const nameMember = document.querySelectorAll('.title');
-        const roleMember = document.querySelectorAll('.role');
-    
-        cardImage[i].innerHTML = arrMemberTeam[i].image
-        nameMember[i].innerHTML = arrMemberTeam[i].name;
-        roleMember[i].innerHTML = arrMemberTeam[i].role;
-    }
+}
 
-});
-
-//// function clear form
-addMemberbtn.addEventListener('click', () => {
-    let inputName = document.querySelector('#name');
-    let inputRole = document.querySelector('#role');
-    let inputImage = document.querySelector('#image');
-
+/// function clear form inputs
+function clearForm () {
     inputName.value = '';
     inputRole.value = '';
     inputImage.value = '';
+}
 
-})
+//// function take property from arrey
+function takepropertyArrey() {
+    for (let i = 0; i < arrMemberTeam.length; i++) {
+
+        const nameMember = document.querySelectorAll('.title');
+        const roleMember = document.querySelectorAll('.role');
+        const cardImage = document.querySelectorAll('.card-image');
+    
+        nameMember[i].innerHTML = arrMemberTeam[i].name;
+        roleMember[i].innerHTML = arrMemberTeam[i].role;
+        cardImage[i].innerHTML = arrMemberTeam[i].image;
+    }
+}
+
+//// function delete card member
+function deleteCard() {
+    arrMemberTeam.splice(this, 1);
+    const index = [...this.parentNode.children].indexOf(this);
+    this.remove();
+}
 
